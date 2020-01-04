5f44c40f4592511112ca08a6f10eec3c8ea93220
Samuel Asher Rivello <sam.rivello@saganworks.com>
2020-01-03 20:56:28 -0400

    Add Spawner System

---
+++ TeatheredDOTS/Assets/Scripts/Runtime/RMC/Projects/TeatheredDOTS/DOTS/ComponentData/SpawnData.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace RMC.Projects.TeatheredDOTS.DOTS.ComponentData
+{
+    public struct SpawnData : IComponentData
+    {
+        public Entity PrefabEntity;
+        public float3 Position;
+        public float3 Velocity;
+    }
+}
